6f968654b165261891f158093204890a1a54821a
vibushnan <vibhushnan.v@blp.co.in>
2024-10-14 14:11:58 +0530

    app.config

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@
 /* eslint-disable semi */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native';
 import UnAuthenticatedTabs from './src/unauthenticaded_screens/unauthenticatedTabs';
